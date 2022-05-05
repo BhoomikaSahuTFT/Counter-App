0a2eac656e3ab35bd4a5f53b36f5bd5c99aee26a
Bhoomika Sahu <sahu.bhoomika@tftus.com>
2022-05-05 17:54:56 +0530

    Counter App

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,22 @@
 import { Component } from "react";
 
 export default class Counter extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       counterValue: 0,
-    }
+    };
   }
-  static getDerivedStateFromProps(props, state){
-    return({
+  static getDerivedStateFromProps(props, state) {
+    return {
       counterValue: props.counterValue,
-    })
+    };
   }
-  render(){
-     return (
-    <>
-      <div className="display">{this.state.counterValue}</div>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <div className="display">{this.state.counterValue}</div>
+      </>
+    );
   }
- 
-};
-
+}
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,5 +1,3 @@
-// This is class based components
-
 import Counter from "./Counter.js";
 import { Component } from "react";
 
@@ -10,37 +8,67 @@ export default class Wrapper extends Component {
       counterValue: 0,
       inputValue: 0,
       isCounter: false,
-      intervalValue: null,
+      intervalID: null,
       isInterval: false,
     };
   }
   inputOnChange = (e) => {
-    this.setState({inputValue: Number(e.target.value)});
+    this.setState({ inputValue: Number(e.target.value) });
     console.log(Number(e.target.value));
   };
-  componentDidMount(){
-    this.setState({isCounter: false})
-  }
-  componentDidUpdate(){
-    this.setState({isCounter: true});
-  }
-  componentWillUnmount(){
-    this.setState({isCounter: true});
-  }
-  startCounter () {
+  incrementCounterValue = () => {
+    this.setState((prevState) => ({
+      counterValue: prevState.counterValue + 1,
+    }));
+  };
+  componentDidMount() {
     this.setState({
+      counterValue: 0,
+      inputValue: 0,
+      isCounter: false,
+      intervalID: null,
+      isInterval: false,
+    });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.isCounter !== this.state.isCounter && this.state.isInterval) {
+      this.setState({
+        intervalID: clearInterval(this.state.intervalID),
+      });
+
+      if (this.state.isCounter) {
+        this.setState({
+          intervalID: setInterval(this.incrementCounterValue, 1000),
+        });
+      }
+    }
+  }
+  componentWillUnmount() {
+    this.setState({
+      intervalID: clearInterval(this.state.intervalID),
+    });
+  }
+  startCounter() {
+    this.setState({
+      isCounter: true,
       isInterval: true,
-    })
-    let counter = setInterval(()=>this.setState({counterValue: this.state.inputValue+1}), 1000);
-    this.setState({
-    })
-    console.log(this.state.inputValue);
+      counterValue: this.state.inputValue,
+      inputValue: 0,
+    });
   }
   stopCounter() {
-
+    this.setState({
+      isCounter: false,
+      isInterval: true,
+    });
   }
   deleteCounter() {
-
+    this.setState({
+      isInterval: false,
+      isCounter: false,
+      counterValue: 0,
+      inputValue: 0,
+    });
   }
   render() {
     return (
@@ -52,18 +80,115 @@ export default class Wrapper extends Component {
           type="number"
           onChange={this.inputOnChange}
         />
-        {/* {isCounter === 0 ? (
-          <></>
-        ) : (
-        )} */}
-
-          <div><Counter counterValue={this.state.counterValue} /></div>
+        {this.state.isInterval && (
+          <div>
+            <Counter counterValue={this.state.counterValue} />
+          </div>
+        )}
         <div className="btn">
-        <button onClick={() => this.startCounter()}>Start</button>
-        <button onClick={() => this.stopCounter()}>Stop</button>
-        <button onClick={() => this.deleteCounter()}>Delete</button>
+          <button onClick={() => this.startCounter()}>Start</button>
+          <button onClick={() => this.stopCounter()}>Stop</button>
+          <button onClick={() => this.deleteCounter()}>Delete</button>
         </div>
       </div>
     );
   }
 }
+
+// import React, { Component } from "react";
+// import Counter from "./Counter";
+// import styles from "../styles/Wrapper.module.css";
+// export class Wrapper extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showCounter: true,
+//       showWelcome: true,
+//       startTimer: false,
+//       startFrom: "",
+//       counterValue: 0,
+//       counterTimer: null,
+//     };
+//   }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ showWelcome: false });
+//     }, 2000);
+//   }
+//   componentWillUnmount() {
+//     clearInterval(this.myInterval);
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.startTimer !== this.state.startTimer) {
+//       if (this.state.counterTimer) {
+//         clearInterval(this.state.counterTimer);
+//       }
+//       if (this.state.startTimer) {
+//         this.setState({
+//           counterTimer: setInterval(this.increaseCounter, 1000),
+//         });
+//       }
+//     }
+//   }
+//   increaseCounter = () => {
+//     this.setState((prevState) => ({
+//       counterValue: prevState.counterValue + 1,
+//     }));
+//   };
+//   startButtonHandler = () => {
+//     this.setState({
+//       counterValue: Number(this.state.startFrom),
+//       startTimer: true,
+//       showCounter: true,
+//       startFrom: "",
+//     });
+//   };
+//   deleteButtonHandler = () => {
+//     this.setState({
+//       counterValue: 0,
+//       startTimer: false,
+//       showCounter: false,
+//       startFrom: "",
+//     });
+//   };
+//   stopButtonHandler = () => {
+//     this.setState({ startTimer: false, startFrom: "" });
+//   };
+//   onchangeHandler = (e) => {
+//     this.setState({ startFrom: Number(e.target.value) });
+//   };
+//   render() {
+//     return (
+//       <div className={styles.container}>
+//         <div className={styles.box}>
+//           {this.state.showWelcome ? (
+//             <h1 className={styles.h1}>Welcome to Counter App</h1>
+//           ) : (
+//             <div className={styles.displayDiv}>
+//               <h1 className={styles.h1}>Counter</h1>
+//               {this.state.showCounter && (
+//                 <Counter value={this.state.counterValue} />
+//               )}
+//               {this.state.showCounter && (
+//                 <input
+//                   placeholder="Enter No..."
+//                   type="number"
+//                   onChange={this.onchangeHandler}
+//                   value={this.state.startFrom}
+//                 />
+//               )}
+//             </div>
+//           )}
+//           {!this.state.showWelcome && (
+//             <div className={styles.buttonDiv}>
+//               <button onClick={this.startButtonHandler}>Start</button>
+//               <button onClick={this.stopButtonHandler}>Stop</button>
+//               <button onClick={this.deleteButtonHandler}>Delete</button>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default Wrapper;
